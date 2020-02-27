8bc9d5c67eb46c22d0098b1d12116107ebb1761f
Brandon Evans <contact@brandonmevans.com>
2020-02-27 01:03:01 -0600

    Allow the deployment of Cougar to an alternative resource group. Modified Cougar to support both Linux and Windows containers. Made the container type configurable with Terraform. Added basic notes on the Windows runtime.

--- cougar/terraform/appServicePlan.tf
+++ cougar/terraform/appServicePlan.tf
@@ -2,7 +2,8 @@ resource "azurerm_app_service_plan" "appServicePlan" {
   name                      = var.AppServicePlanName
   location                  = var.ResourceGroupLocation
   resource_group_name       = azurerm_resource_group.cougar.name
-  kind                      = "FunctionApp"
+  kind                      = var.AppServicePlanKind
+  reserved                  = lower(var.AppServicePlanKind) == "linux" ? true : false
 
   sku {
     tier = "Dynamic"
