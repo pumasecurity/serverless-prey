93d3098f664e6d79c8d63ccfc6e3a3c2ec431ce5
Eric Johnson <eric.johnson@pumasecurity.io>
2023-08-26 23:02:35 -0400

    bug: app service plan

--- cougar/terraform/appServicePlan.tf
+++ cougar/terraform/appServicePlan.tf
@@ -1,12 +1,12 @@
 resource "azurerm_app_service_plan" "appServicePlan" {
-  name                      = var.AppServicePlanName
-  location                  = azurerm_resource_group.cougar.location
-  resource_group_name       = azurerm_resource_group.cougar.name
-  kind                      = var.AppServicePlanKind
-  reserved                  = lower(var.AppServicePlanKind) == "linux" ? true : false
+  name                = var.AppServicePlanName
+  location            = azurerm_resource_group.cougar.location
+  resource_group_name = azurerm_resource_group.cougar.name
+  kind                = var.AppServicePlanKind
+  reserved            = lower(var.AppServicePlanKind) == "linux" ? true : false
 
   sku {
-    tier = "Basic"
-    size = "B1"
+    tier = "Dynamic"
+    size = "Y1"
   }
 }
