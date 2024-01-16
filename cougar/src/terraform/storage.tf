eae9474eee0118e9baec46634ab272531a2afde9
Eric Johnson <eric.johnson@pumasecurity.io>
2024-01-16 11:24:46 -0600

    feat: static function zip

--- cougar/src/terraform/storage.tf
+++ cougar/src/terraform/storage.tf
@@ -30,52 +30,12 @@ resource "random_uuid" "courgar_storage_flag" {
   count = var.configure_ctf ? 1 : 0
 }
 
-data "template_file" "cougar_config" {
-  template = file("${path.module}/../../assets/appsettings.tpl")
-  vars = {
-    database_password = var.configure_ctf ? "${var.flag_prefix}{${random_uuid.cougar_config_flag[0].id}}" : "QnV0IHVuaWNvcm5zIGFwcGFyZW50bHkgZG8gZXhpc3Qu"
-    storage_account   = "cougar${var.configure_ctf ? random_string.cougar_config_name[0].id : var.unique_identifier}"
-    storage_container = "assets"
-  }
-}
-
-resource "local_file" "cougar_config" {
-  filename = "${path.module}/../cougar/appsettings.json"
-  content  = data.template_file.cougar_config.rendered
-}
-
-resource "null_resource" "cougar_build" {
-  depends_on = [
-    azurerm_storage_container.function_cougar,
-    local_file.cougar_config,
-  ]
-
-  provisioner "local-exec" {
-    working_dir = "${path.module}/../cougar"
-    command     = <<-EOT
-        dotnet publish --output "./../publish" --configuration "Release" /p:GenerateRuntimeConfigurationFiles=true --self-contained false
-    EOT
-  }
-
-  triggers = {
-    deploy = timestamp()
-  }
-}
-
-data "archive_file" "cougar_build" {
-  depends_on = [null_resource.cougar_build]
-
-  type        = "zip"
-  source_dir  = "${path.module}/../publish"
-  output_path = "${path.module}/../functionapp.zip"
-}
-
 resource "azurerm_storage_blob" "function_cougar" {
   name                   = "functionapp.zip"
   storage_account_name   = azurerm_storage_account.function.name
   storage_container_name = azurerm_storage_container.function_cougar.name
   type                   = "Block"
-  source                 = data.archive_file.cougar_build.output_path
+  source                 = "${path.module}/../functionapp.zip"
 }
 
 resource "azurerm_storage_account" "assets" {
